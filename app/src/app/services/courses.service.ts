fd96be2261bf808d9a36eac3a7e9216021bb546c
Eduardo Tavares <edutav@gmail.com>
2020-11-24 15:47:02 -0400

    serviços de comunicação com a api

---
+++ app/src/app/services/courses.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:3000/courses';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  get(id): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  delete(id): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+}
